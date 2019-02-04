7db9749d63d2647fd84184aef2a353817ceb11eb
glweems <gwgraphicdesign@gmail.com>
2019-02-04 15:06:38 -0500

    rename

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import SudoUi from './sudo-ui.js'
+import Library from './library'
 
-Vue.use(Router, SudoUi)
+Vue.use(Router, Library)
 
 export default new Router({
   mode: 'history',
