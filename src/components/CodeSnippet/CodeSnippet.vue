d103e0ddf0176c7c6d53bc81b7a84721545d61d2
glweems <gwgraphicdesign@gmail.com>
2019-02-08 16:46:49 -0500

    updates

---
+++ src/components/CodeSnippet/CodeSnippet.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="CodeSnippet">
+		<p v-if="title">{{ title }}</p>
+		<pre>
+<code>
+<slot/></code>
+		</pre>
+	</div>
+</template> 
+
+<script>
+export default {
+	name: "CodeSnippet",
+	props: { title: { type: String, required: false } }
+};
+</script>
+
+ <style lang="scss" scoped>
+	pre {
+		background: white;
+		padding: 0 1rem;
+		border-radius: 0.1rem;
+		code {
+			color: black;
+		}
+	}
+</style>
--- src/components/hero/Hero.vue
+++ src/components/hero/Hero.vue
@@ -1,29 +1,42 @@
 <template>
-  <div class="hero">
-    <h1 class="heading">{{ title }}</h1>
-    <h4 class="subtitle">{{ subtitle }}</h4>
-    <p class="msg">{{ msg }}</p>
-  </div>
+	<div :class="active">
+		<h1 class="heading">{{ title }}</h1>
+		<h3 class="subtitle">{{ subtitle }}</h3>
+		<p class="msg">{{ msg }}</p>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Hero",
-  props: {
-    title: { type: String },
-    subtitle: { type: String },
-    msg: { type: String }
-  }
+	name: "Hero",
+	props: {
+		title: { type: String },
+		subtitle: { type: String },
+		msg: { type: String },
+		left: { type: Boolean },
+		right: { type: Boolean }
+	},
+	beforeMount() {
+		this.findOptions();
+	},
+	data() {
+		return {
+			active: ["hero"]
+		};
+	},
+	methods: {
+		findOptions() {
+			if (this.left) {
+				this.setOption("left");
+			}
+			if (this.right) {
+				this.setOption("right");
+			}
+		},
+		setOption(option) {
+			this.active.push(option);
+		}
+	}
 };
 </script>
 
-<style lang="scss" scoped>
-@import "sudo-styling/scss/sudo-styling.scss";
-.hero {
-  text-align: center;
-  @extend %container-fluid;
-  @extend %border-radius;
-  @include background(dark);
-  @include color(light);
-}
-</style>
